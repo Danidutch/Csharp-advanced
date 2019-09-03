d8fe3a88a8a1c20e626d168ed73c521e05a49421
D F T <42302378+Danidutch@users.noreply.github.com>
2019-09-03 21:31:59 +0200

    Classes Private, Public and  Protected

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Hello
+{
+    class Program
+    {
+        class Shape
+        {
+
+            private float Volume = 0;
+
+            public void SetVolume(float val)
+            {
+                Volume =val;
+            }
+            public float GetVolume()
+            {
+                return Volume;
+            }
+        }
+    
+
+
+    
+        static void Main(string[] args)
+        {
+
+            Shape a = new Shape();
+            a.SetVolume(2.3f);
+            Console.WriteLine("a Volume: " + a.GetVolume());
+            Console.ReadLine();
+        }
+    }
+}
+    
+
