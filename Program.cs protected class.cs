05e8688f2bc74f1477cac2918b98767b9f38d626
D F T <42302378+Danidutch@users.noreply.github.com>
2019-09-03 21:57:24 +0200

    Protected class

---
+++ Program.cs protected class.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Hello
+{
+    class Program
+    {
+        class Shape
+        {
+
+            protected float Volume = 0;
+
+            public void SetVolume(float val)
+            {
+                Volume =val;
+            }
+            public float GetVolume()
+            {
+                return Volume;
+            }
+        }
+    
+
+
+        class Rect : Shape
+        {
+
+
+
+            static void Main(string[] args)
+            {
+
+                Shape a = new Shape();
+                a.SetVolume(2.3f);
+                Rect b = new Rect();
+                b.SetVolume(5.9f);
+
+                Console.WriteLine("Child of protected class. Volume: " + b.Volume);
+               Console.WriteLine("a Volume: " + a.GetVolume());
+               Console.ReadLine();
+            }
+        }
+    }
+}
+    
+
