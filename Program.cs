21c64c8487f46e898b2ac8b40559016b3343567b
D F T <42302378+Danidutch@users.noreply.github.com>
2019-09-04 09:32:15 +0200

    Constructor and destructor program.cs

--- Program.cs
+++ Program.cs
@@ -4,37 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Advanced_Hello
+namespace Construtor_and_Destructor
 {
     class Program
     {
-        class Shape
-        {
-
-            private float Volume = 0;
-
-            public void SetVolume(float val)
-            {
-                Volume =val;
-            }
-            public float GetVolume()
-            {
-                return Volume;
-            }
-        }
-    
-
-
-    
         static void Main(string[] args)
         {
-
-            Shape a = new Shape();
-            a.SetVolume(2.3f);
-            Console.WriteLine("a Volume: " + a.GetVolume());
-            Console.ReadLine();
         }
     }
 }
-    
-
